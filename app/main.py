6995f3f0342adf06f387a34d4cd7fd786e9657ff
Arthur Garcia Takahashi <arthurgt@alumni.usp.br>
2024-05-18 03:58:51 -0300

    teste producao

--- app/main.py
+++ app/main.py
@@ -1,13 +1,20 @@
 # app/main.py
 from fastapi import FastAPI
-from .routes.jogadores import router as jogadores_router
+# from .routes.jogadores import router as jogadores_router
+
+from .routes import jogadores
 
 app = FastAPI()
 
-app.include_router(jogadores_router, prefix="/jogadores", tags=["jogadores"])
+# app.include_router(jogadores_router, prefix="/jogadores", tags=["jogadores"])
+app.include_router(jogadores.router)
 
 @app.get("/")
 def read_root():
     return {"message": "Running!"}
 
 
+
+# if __name__ == '__main__':
+#     import uvicorn
+#     uvicorn.run(app, host='0.0.0.0', port=8000)
