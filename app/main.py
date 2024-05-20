6bc6e1427b258b28bca20667290c95e92e7d13d0
Arthur Garcia Takahashi <arthurgt@alumni.usp.br>
2024-05-19 21:53:44 -0300

    ajuste port

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 # from .routes.jogadores import router as jogadores_router
 
 from .routes import jogadores
-
+import os
 app = FastAPI()
 
 # app.include_router(jogadores_router, prefix="/jogadores", tags=["jogadores"])
@@ -14,7 +14,7 @@ def read_root():
     return {"message": "Running!"}
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host='0.0.0.0', port=8000, reload=True)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
